497245c373dfaf03b77081e062cf40c2af7b7dfa
RoachxD <roachxxxx@gmail.com>
2015-01-28 04:01:19 +0200

    Dunno  - Removed Unused References.  - Re-indented the Script.

--- Unban.exe/Unban.exe/Program.cs
+++ Unban.exe/Unban.exe/Program.cs
@@ -1,7 +1,5 @@
-﻿
-using System;
+﻿using System;
 using System.Drawing;
-using System.IO;
 using System.Net;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -10,25 +8,28 @@ using SharpDX.Direct3D9;
 
 namespace Unban.exe
 {
-    class Program
+    internal class Program
     {
-        public static SharpDX.Direct3D9.Texture taco;
-        public static SharpDX.Direct3D9.Sprite sprite;
-        public static SharpDX.Direct3D9.Device dxDevice = Drawing.Direct3DDevice;
-        public static int i = 0;
-        static void Main(string[] args)
+        public static Texture Taco;
+        public static Sprite Sprite;
+        public static Device DxDevice = Drawing.Direct3DDevice;
+        public static int I;
+
+        private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        static void Game_OnGameLoad(EventArgs args)
+        private static void Game_OnGameLoad(EventArgs args)
         {
             Game.PrintChat("Unban.exe By DZ191 Loaded. Credits to DETUKS");
-            sprite = new Sprite(dxDevice);
-           taco = Texture.FromMemory(
-                    Drawing.Direct3DDevice,
-                    (byte[])new ImageConverter().ConvertTo(LoadPicture("http://puu.sh/cP1qD/d23cd24220.jpg"), typeof(byte[])), 513, 744, 0,
-                    Usage.None, Format.A1, Pool.Managed, Filter.Default, Filter.Default, 0);
+            Sprite = new Sprite(DxDevice);
+            Taco = Texture.FromMemory(
+                Drawing.Direct3DDevice,
+                (byte[])
+                    new ImageConverter().ConvertTo(LoadPicture("http://puu.sh/cP1qD/d23cd24220.jpg"), typeof(byte[])),
+                513, 744, 0, Usage.None, Format.A1, Pool.Managed, Filter.Default, Filter.Default, 0);
+
             Drawing.OnEndScene += Drawing_OnEndScene;
             Drawing.OnPreReset += DrawingOnOnPreReset;
             Drawing.OnPostReset += DrawingOnOnPostReset;
@@ -38,45 +39,51 @@ namespace Unban.exe
 
         private static void CurrentDomainOnDomainUnload(object sender, EventArgs e)
         {
-            sprite.Dispose();
+            Sprite.Dispose();
         }
 
         private static void DrawingOnOnPostReset(EventArgs args)
         {
-            sprite.OnResetDevice();
+            Sprite.OnResetDevice();
         }
 
         private static void DrawingOnOnPreReset(EventArgs args)
         {
-            sprite.OnLostDevice();
+            Sprite.OnLostDevice();
         }
+
         private static Bitmap LoadPicture(string url)
         {
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var responseStream = response.GetResponseStream();
 
-            System.Net.WebRequest request = System.Net.WebRequest.Create(url);
-            System.Net.WebResponse response = request.GetResponse();
-            System.IO.Stream responseStream = response.GetResponseStream();
-            Bitmap bitmap2 = new Bitmap(responseStream);
+            var bitmap2 = new Bitmap(responseStream);
             Console.WriteLine(bitmap2.Size);
+
             return (bitmap2);
         }
 
-        static void Drawing_OnEndScene(EventArgs args)
+        private static void Drawing_OnEndScene(EventArgs args)
         {
-           DrawTaco();
+            DrawTaco();
         }
 
-        static void DrawTaco()
+        private static void DrawTaco()
         {
-            if (i == 360) i = 0;
-            var IAngle = i*(Math.PI/180);
-            var Sin = (float)Math.Sin(IAngle);
-            var Cos = (float)Math.Cos(IAngle);
-            sprite.Begin();
-            sprite.Draw(taco, new ColorBGRA(255, 255, 255, 255), null, new Vector3(-450+200*Cos, 0+200*Sin, 0));
-            sprite.End();
-            i+=5;
-           // Utility.DelayAction.Add(15, () => DrawTaco());
+            if (I == 360)
+            {
+                I = 0;
+            }
+
+            var angle = I * (Math.PI / 180);
+            var sin = (float) Math.Sin(angle);
+            var cos = (float) Math.Cos(angle);
+            Sprite.Begin();
+            Sprite.Draw(Taco, new ColorBGRA(255, 255, 255, 255), null, new Vector3(-450 + 200 * cos, 0 + 200 * sin, 0));
+            Sprite.End();
+            I += 5;
+            // Utility.DelayAction.Add(15, () => DrawTaco());
         }
     }
 }
