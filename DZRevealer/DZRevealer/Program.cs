95aaeee098f40a889294c7d6333667aaaf251d46
RoachxD <roachxxxx@gmail.com>
2015-01-28 03:23:04 +0200

    Little Update  - Removed Unused References.  - Simplified and Improved most of the Script.

--- DZRevealer/DZRevealer/Program.cs
+++ DZRevealer/DZRevealer/Program.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Input;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-
-using Color = System.Drawing.Color;
 
 namespace DZRevealer
 {
     //Inspired from the discontinued BoL script Disclosures. Many thanks to the Autor for his Trinket\Ward place calcs.
 
-    class Program
+    internal class Program
     {
-        public static Dictionary<String, String> dict;
-        public static Obj_AI_Base player = ObjectManager.Player;
+        public static Dictionary<String, String> Dict;
+        public static Obj_AI_Base Player = ObjectManager.Player;
         public static Spell E;
-        public static Menu menu;
-        public static int VISION_WARD = 2043;
-        public static int TRINKET_RED = 3364;
-        public static float wardrange = 600f;
-        public static float trinket_range = 600f;
-        public static bool debug = false;
-        static void Main(string[] args)
+        public static Menu Menu;
+        public static int VisionWard = 2043;
+        public static int TrinketRed = 3364;
+        public static float WardRange = 600f;
+        public static float TrinketRange = 600f;
+        public static bool Debug = false;
+
+        private static void Main(string[] args)
         {
             try
             {
@@ -32,115 +30,116 @@ namespace DZRevealer
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
-                return;
             }
-            
         }
-        static void Game_OnGameLoad(EventArgs args)
+
+        private static void Game_OnGameLoad(EventArgs args)
         {
-            menu = new Menu("DZReveal!", "DZReveal", true);
-            menu.AddItem(new MenuItem("doRev", "Reveal").SetValue(true));
-            menu.AddItem(new MenuItem("revDesc1", "Priority:"));
-            menu.AddItem(new MenuItem("prior", "ON: Pink OFF: Trinket").SetValue(true));
-            if(player.BaseSkinName =="LeeSin")
+            Menu = new Menu("DZReveal!", "DZReveal", true);
+            Menu.AddItem(new MenuItem("doRev", "Reveal").SetValue(true));
+            Menu.AddItem(new MenuItem("revDesc1", "Priority:"));
+            Menu.AddItem(new MenuItem("prior", "ON: Pink OFF: Trinket").SetValue(true));
+            if (Player.BaseSkinName == "LeeSin")
             {
-                menu.AddItem(new MenuItem("leeE", "Lee Sin: Use E").SetValue(true));
+                Menu.AddItem(new MenuItem("leeE", "Lee Sin: Use E").SetValue(true));
                 E = new Spell(SpellSlot.E, 350f);
             }
+
             Game.PrintChat("DZReveal Loaded");
-            menu.AddToMainMenu();
-            fillDict();
-            Game.PrintChat(player.BaseSkinName);
-            Game.OnGameProcessPacket += Game_OnGameProcessPacket;
-            Game.OnGameUpdate += Game_GameUpdate;
+            Menu.AddToMainMenu();
+            FillDict();
+            Game.PrintChat(Player.BaseSkinName);
+            Game.OnGameUpdate += GameGameUpdate;
         }
 
-        private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
+        private static void GameGameUpdate(EventArgs args)
         {
-        }
-        public static void OnGainBuff(Obj_AI_Hero unit,String buff)
-        {
-
-        }
-        private static void Game_GameUpdate(EventArgs args)
-        {
-            if (!isEn("doRev")) return;
-            foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy))
+            if (!IsEn("doRev"))
             {
-                if(enemy.HasBuffOfType(BuffType.Invisibility) && !(enemy.BaseSkinName =="Evelynn"))
-                {
-                    Reveal(enemy);
-                }
+                return;
             }
 
+            foreach (
+                var enemy in
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(hero => hero.IsEnemy)
+                        .Where(enemy => enemy.HasBuffOfType(BuffType.Invisibility) && enemy.BaseSkinName != "Evelynn"))
+            {
+                Reveal(enemy);
+            }
         }
 
-        static void Reveal(Obj_AI_Hero enemy)
+        private static void Reveal(Obj_AI_Hero enemy)
         {
-            if(player.BaseSkinName == "LeeSin" && E.IsReady() && player.Distance(enemy)<= E.Range && isEn("leeE"))
+            if (Player.BaseSkinName == "LeeSin" && E.IsReady() && Player.Distance(enemy) <= E.Range && IsEn("leeE"))
             {
                 E.Cast();
             }
             else
             {
-            if (isEn("prior"))
-            {
-                //W
-                if (player.Distance(enemy) <= wardrange+300f)
+                if (IsEn("prior"))
                 {
-                    if(player.Distance(enemy) <= wardrange)
+                    //W
+                    if (!(Player.Distance(enemy) <= WardRange + 300f))
                     {
-                        useItem(VISION_WARD, enemy.Position);
+                        return;
                     }
-                    else
-                    {       
-                        Vector3 pos1 = Vector3.Lerp(player.Position, enemy.Position, wardrange / player.Distance(enemy));
-                        useItem(VISION_WARD, pos1);
-                    }
-                    
-                }
-            }
-            else
-            {
-                //Trink
-                if (player.Distance(enemy) <= trinket_range+300f)
-                {
-                    if (player.Distance(enemy) <= trinket_range)
+
+                    if (Player.Distance(enemy) <= WardRange)
                     {
-                        useItem(TRINKET_RED, enemy.Position);
+                        UseItem(VisionWard, enemy.Position);
                     }
                     else
                     {
-                        Vector3 pos1 = Vector3.Lerp(player.Position, enemy.Position, trinket_range / player.Distance(enemy));
-                        useItem(TRINKET_RED, pos1);
+                        var pos1 = Vector3.Lerp(Player.Position, enemy.Position, WardRange / Player.Distance(enemy));
+                        UseItem(VisionWard, pos1);
                     }
-                    
+                }
+                else
+                {
+                    //Trink
+                    if (!(Player.Distance(enemy) <= TrinketRange + 300f))
+                    {
+                        return;
+                    }
+
+                    if (Player.Distance(enemy) <= TrinketRange)
+                    {
+                        UseItem(TrinketRed, enemy.Position);
+                    }
+                    else
+                    {
+                        var pos1 = Vector3.Lerp(Player.Position, enemy.Position, TrinketRange / Player.Distance(enemy));
+                        UseItem(TrinketRed, pos1);
+                    }
                 }
             }
-            }
-            
         }
-        public static bool isEn(String item)
+
+        public static bool IsEn(String item)
         {
-            return menu.Item(item).GetValue<bool>();
+            return Menu.Item(item).GetValue<bool>();
         }
-        
-        static void fillDict()
+
+        private static void FillDict()
         {
-            dict = new Dictionary<String, String>();
-            dict.Add("Vayne", "VayneTumbleFade");
-            dict.Add("Twitch", "TwitchHideInShadows");
-            dict.Add("Rengar", "RengarR");
-            dict.Add("MonkeyKing", "monkeykingdecoystealth");
-            dict.Add("Khazix", "khazixrstealth");
-            dict.Add("Talon", "talonshadowassaultbuff");
-            dict.Add("Akali", "akaliwstealth");
+            Dict = new Dictionary<String, String>
+            {
+                { "Vayne", "VayneTumbleFade" },
+                { "Twitch", "TwitchHideInShadows" },
+                { "Rengar", "RengarR" },
+                { "MonkeyKing", "monkeykingdecoystealth" },
+                { "Khazix", "khazixrstealth" },
+                { "Talon", "talonshadowassaultbuff" },
+                { "Akali", "akaliwstealth" }
+            };
         }
-        public static void useItem(int id, Vector3 position)
+
+        public static void UseItem(int id, Vector3 position)
         {
             if (Items.HasItem(id) && Items.CanUseItem(id))
             {
-                Items.UseItem(id,position);
+                Items.UseItem(id, position);
             }
         }
     }
