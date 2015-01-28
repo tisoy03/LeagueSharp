50129204ce536fff170305a9d61b1b0b080ee198
RoachxD <roachxxxx@gmail.com>
2015-01-28 03:38:12 +0200

    Updaterino!  - Removed Unused References.  - Re-indented the Script.

--- MinionHPBar/MinionHPBar/Program.cs
+++ MinionHPBar/MinionHPBar/Program.cs
@@ -4,10 +4,10 @@ using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
 using Color = System.Drawing.Color;
+
 namespace MinionHPBar
 {
-
-    class Program
+    internal class Program
     {
         public static Obj_AI_Base Player = ObjectManager.Player;
         public static Menu Menu;
@@ -23,19 +23,23 @@ namespace MinionHPBar
             Menu.AddSubMenu(new Menu("Drawing", "Drawing"));
 
             //Drawings Menu
-            Menu.SubMenu("Drawing").AddItem(new MenuItem("DrawLines", "Draw Bars").SetValue(new Circle(true,Color.Black)));
-            Menu.SubMenu("Drawing").AddItem(new MenuItem("LastHitH", "LastHit Helper").SetValue(new Circle(true,Color.Lime)));
-            Menu.SubMenu("Drawing").AddItem(new MenuItem("Thick", "Thickness").SetValue(new Slider(2,1,3)));
-            Menu.SubMenu("Drawing").AddItem(new MenuItem("NearDead", "Nearly dead Circle").SetValue(new Circle(true, Color.Green)));
-            Menu.SubMenu("Drawing").AddItem(new MenuItem("1Hit", "LastHit Circle").SetValue(new Circle(true, Color.Coral)));
+            Menu.SubMenu("Drawing")
+                .AddItem(new MenuItem("DrawLines", "Draw Bars").SetValue(new Circle(true, Color.Black)));
+            Menu.SubMenu("Drawing")
+                .AddItem(new MenuItem("LastHitH", "LastHit Helper").SetValue(new Circle(true, Color.Lime)));
+            Menu.SubMenu("Drawing").AddItem(new MenuItem("Thick", "Thickness").SetValue(new Slider(2, 1, 3)));
+            Menu.SubMenu("Drawing")
+                .AddItem(new MenuItem("NearDead", "Nearly dead Circle").SetValue(new Circle(true, Color.Green)));
+            Menu.SubMenu("Drawing")
+                .AddItem(new MenuItem("1Hit", "LastHit Circle").SetValue(new Circle(true, Color.Coral)));
             //Range Menu
             Menu.AddSubMenu(new Menu("Ranges", "Range"));
-            Menu.SubMenu("Range").AddItem(new MenuItem("DrRange", "Draw Range").SetValue(new Slider(850,450,1200)));
+            Menu.SubMenu("Range").AddItem(new MenuItem("DrRange", "Draw Range").SetValue(new Slider(850, 450, 1200)));
             Menu.AddToMainMenu();
 
             //Credits
             Game.PrintChat("MinionHPBars by DZ191");
-            
+
             //Events
             Drawing.OnDraw += Drawing_OnDraw;
         }
@@ -43,40 +47,55 @@ namespace MinionHPBar
         private static void Drawing_OnDraw(EventArgs args)
         {
             if (!Menu.Item("DrawLines").GetValue<Circle>().Active)
-                return;
-            var minionList = MinionManager.GetMinions(Player.Position, Menu.Item("DrRange").GetValue<Slider>().Value, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.MaxHealth);
-            foreach (var minion in minionList.Where(minion => minion.IsValidTarget(Menu.Item("DrRange").GetValue<Slider>().Value)))
             {
-                var attackToKill  = Math.Ceiling(minion.MaxHealth/Player.GetAutoAttackDamage(minion, true));
+                return;
+            }
+
+            var minionList = MinionManager.GetMinions(
+                Player.Position, Menu.Item("DrRange").GetValue<Slider>().Value, MinionTypes.All, MinionTeam.Enemy,
+                MinionOrderTypes.MaxHealth);
+            foreach (
+                var minion in
+                    minionList.Where(minion => minion.IsValidTarget(Menu.Item("DrRange").GetValue<Slider>().Value)))
+            {
+                var attackToKill = Math.Ceiling(minion.MaxHealth / Player.GetAutoAttackDamage(minion, true));
                 var hpBarPosition = minion.HPBarPosition;
                 var barWidth = minion.IsMelee() ? 75 : 80; //Width for melee and ranged
                 if (minion.HasBuff("turretshield", true)) //Cannon minion
                 {
                     barWidth = 70;
                 }
-                var barDistance = barWidth/attackToKill;
+
+                var barDistance = barWidth / attackToKill;
                 for (var i = 0; i < attackToKill; i++)
                 {
                     if (i != 0)
                     {
                         Drawing.DrawLine(
-                            new Vector2(hpBarPosition.X + 45 + (float) (barDistance)*i, hpBarPosition.Y + 18),
-                            new Vector2(hpBarPosition.X + 45 + ((float) (barDistance)*i), hpBarPosition.Y + 23), Menu.Item("Thick").GetValue<Slider>().Value,
-                            ((minion.Health <= Player.GetAutoAttackDamage(minion, true) && Menu.Item("LastHitH").GetValue<Circle>().Active) ? Menu.Item("LastHitH").GetValue<Circle>().Color : Menu.Item("DrawLines").GetValue<Circle>().Color));
+                            new Vector2(hpBarPosition.X + 45 + (float) (barDistance) * i, hpBarPosition.Y + 18),
+                            new Vector2(hpBarPosition.X + 45 + ((float) (barDistance) * i), hpBarPosition.Y + 23),
+                            Menu.Item("Thick").GetValue<Slider>().Value,
+                            ((minion.Health <= Player.GetAutoAttackDamage(minion, true) &&
+                              Menu.Item("LastHitH").GetValue<Circle>().Active)
+                                ? Menu.Item("LastHitH").GetValue<Circle>().Color
+                                : Menu.Item("DrawLines").GetValue<Circle>().Color));
                     }
                 }
+
                 //Based on LXOrbwalker. Thanks Lexxes
                 if (Menu.Item("1Hit").GetValue<Circle>().Active &&
-                        minion.Health <= Player.GetAutoAttackDamage(minion, true))
-                    Utility.DrawCircle(minion.Position, minion.BoundingRadius, Menu.Item("1Hit").GetValue<Circle>().Color);
+                    minion.Health <= Player.GetAutoAttackDamage(minion, true))
+                {
+                    Utility.DrawCircle(
+                        minion.Position, minion.BoundingRadius, Menu.Item("1Hit").GetValue<Circle>().Color);
+                }
                 else if (Menu.Item("NearDead").GetValue<Circle>().Active &&
                          minion.Health <= Player.GetAutoAttackDamage(minion, true) * 2)
-                    Utility.DrawCircle(minion.Position, minion.BoundingRadius, Menu.Item("NearDead").GetValue<Circle>().Color);
-             
+                {
+                    Utility.DrawCircle(
+                        minion.Position, minion.BoundingRadius, Menu.Item("NearDead").GetValue<Circle>().Color);
+                }
             }
-
-            
         }
-        
     }
 }
