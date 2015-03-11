b13c4eb47c9aadcbde30d217ab8de8ad156aacfa
DZ191 <darkprince97@live.it>
2015-03-11 18:21:39 +0100

    DZAIO: Added Heal.cs as a summoner spell

---
+++ DZAIO/Utility/Spells/Heal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DZAIO.Utility.Helpers;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace DZAIO.Utility.Spells
+{
+    class Heal : ISummonerSpell
+    {
+        public void OnLoad(){}
+        public string GetDisplayName()
+        {
+            return "Heal";
+        }
+
+        public void AddToMenu(Menu menu)
+        {
+            menu.AddItem(
+                new MenuItem("daio.activator.summonerspells." + GetName() + ".hpercent", "Health %").SetValue(
+                    new Slider(25, 1)));
+        }
+
+        public bool RunCondition()
+        {
+            return GetSummonerSpell().IsReady() &&
+                   MenuHelper.isMenuEnabled("dzaio.activator.summonerspells." + GetName() + ".enabled") &&
+                   ObjectManager.Player.HealthPercentage() <=
+                   MenuHelper.getSliderValue("daio.activator.summonerspells." + GetName() + ".hpercent");
+        }
+
+        public void Execute()
+        {
+                GetSummonerSpell().Cast();
+        }
+
+        public SummonerSpell GetSummonerSpell()
+        {
+            return SummonerSpells.Heal;
+        }
+
+        public string GetName()
+        {
+            return GetSummonerSpell().Names.First().ToLowerInvariant();
+        }
+    }
+}
