41182a811026dc2e4f2d37cef8561d3a9ef3a9ef
DZ191 <darkprince97@live.it>
2015-07-02 10:52:26 +0200

    Thresh: First Push

---
+++ Thresh - Soul Hunter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Thresh___Soul_Hunter
+{
+    using LeagueSharp;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeagueSharp.SDK.Core.Events.Load.OnLoad += (sender, eventArgs) =>
+            {
+                if (ObjectManager.Player.ChampionName.Equals("Thresh"))
+                {
+                    Thresh.OnLoad();
+                }
+            };
+        }
+    }
+}
