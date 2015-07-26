ac61b2af1523835fea69c1bbdf7be17922134381
DZ191 <darkprince97@live.it>
2015-07-26 18:11:24 +0200

    VH Revolution: Fixed Version Number & Stuff.

--- VHR_SDK/Properties/AssemblyInfo.cs
+++ VHR_SDK/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VHR_SDK")]
+[assembly: AssemblyTitle("Vayne Hunter Revolution")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("VHR_SDK")]
+[assembly: AssemblyCompany("LeagueSharp")]
+[assembly: AssemblyProduct("Vayne Hunter Revolution")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
