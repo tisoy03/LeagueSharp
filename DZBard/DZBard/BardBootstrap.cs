67bf70e3bbb64986520e7172834f85a127c97988
DZ191 <darkprince97@live.it>
2015-08-27 20:37:03 +0200

    Bard: Something.

---
+++ DZBard/DZBard/BardBootstrap.cs
@@ -0,0 +1,21 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace DZBard
+{
+    class BardBootstrap
+    {
+        internal static void OnLoad()
+        {
+            if (ObjectManager.Player.ChampionName != "Bard")
+            {
+                return;
+            }
+
+            Bard.BardMenu = new Menu("Bard","dz191.bard", true);
+
+            MenuGenerator.OnLoad(Bard.BardMenu);
+            Bard.OnLoad();
+        }
+    }
+}
---
+++ DZBard/DZBard/MenuGenerator.cs
@@ -0,0 +1,28 @@
+﻿using LeagueSharp.Common;
+
+namespace DZBard
+{
+    class MenuGenerator
+    {
+        internal static void OnLoad(Menu rootMenu)
+        {
+            var OrbwalkerMenu = new Menu("Orbwalker","dz191.bard.orbwalker");
+            Bard.BardOrbwalker = new Orbwalking.Orbwalker(OrbwalkerMenu);
+            rootMenu.AddSubMenu(OrbwalkerMenu);
+
+            TargetSelector.AddToMenu(rootMenu);
+
+            var comboMenu = new Menu("Combo","dz191.bard.combo");
+            {
+                comboMenu.AddItem(new MenuItem("dz191.bard.combo.useq", "Use Q").SetValue(true));
+                comboMenu.AddItem(new MenuItem("dz191.bard.combo.distance", "Calculation distance").SetValue(new Slider(250, 100, 450)));
+                comboMenu.AddItem(new MenuItem("dz191.bard.combo.accuracy", "Accuracy").SetValue(new Slider(45, 1)));
+
+            }
+
+            //All Bard needs for now is Q amk
+
+            rootMenu.AddToMainMenu();
+        }
+    }
+}
