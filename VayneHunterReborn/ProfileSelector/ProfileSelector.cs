7d96f51b33a0890509cf9f506fc13ed35397e2b0
DZ191 <darkprince97@live.it>
2015-09-07 10:10:42 +0200

    VHR: Profiler first push.

---
+++ VayneHunterReborn/ProfileSelector/ProfileSelector.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VayneHunter_Reborn.ProfileSelector
+{
+    class ProfileSelector
+    {
+
+    }
+
+    internal class ProfileSettings
+    {
+        public String ProfileName { get; set; }
+
+        public List<ProfileOption> Options { get; set; } 
+    }
+
+    internal class ProfileOption
+    {
+        public MajorCategories MajorCategory { get; set; }
+
+        public MinorCategories MinorCategory { get; set; }
+
+        public String ExtraOptions { get; set; }
+
+        public Value Value { get; set; }
+
+    }
+
+    internal enum MajorCategories
+    {
+        Tumble, Condemn, General
+    }
+
+    internal enum MinorCategories
+    {
+        QLogic, SmartQ, NoAAStealth, NoQEnemies, DynamicQSafety, QSpam, QInRange, Mirin, WallTumble
+
+    }
+
+    internal enum ValueTypes
+    {
+        Keybind, Boolean, Slider
+    }
+
+    abstract class Value
+    {
+        public abstract ValueTypes GetValueType();
+
+        public abstract T GetValue<T>();
+
+    }
+
+    /**
+    internal class SliderValue : Value
+    {
+        public override ValueTypes GetValueType()
+        {
+            return ValueTypes.Slider;
+        }
+
+        public override T GetValue<T>()
+        {
+            return null;
+            //return new Tuple<int, int, int>(0, 0, 0);
+        }
+    }
+    */
+}
