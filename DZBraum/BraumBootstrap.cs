d4f685e1dd5b8ec3942441214cac04da02b4f83c
DZ191 <darkprince97@live.it>
2015-09-15 18:51:57 +0200

    Braum: Base

---
+++ DZBraum/BraumBootstrap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DZBraum.Utility;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace DZBraum
+{
+    class BraumBootstrap
+    {
+        public static void OnLoad()
+        {
+            if (ObjectManager.Player.ChampionName != "Braum")
+            {
+                return;
+            }
+            Braum.BraumMenu = new Menu("Braum - Something","dz191.braum", true);
+            MenuGenerator.OnLoad(Braum.BraumMenu);
+        }
+    }
+}
